a1f70d526a98173b973284373990390ead0b2e62
Andrew Grant <andrew.grant@uinsure.co.uk>
2024-06-16 19:29:20 +0100

    refactor: Use lift up conditional refactoring pattern to consolidate aged brie behaviour

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -15,55 +15,65 @@ public class GildedRose(IList<Item> Items)
 
     private static void UpdateItem(Item item)
     {
-        if (item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert")
+        if (item.Name == "Aged Brie")
         {
             if (item.Quality < 50)
             {
                 item.Quality += 1;
-
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.SellIn < 11)
-                    {
-                        IncreaseQuality(item);
-                    }
-
-                    if (item.SellIn < 6)
-                    {
-                        IncreaseQuality(item);
-                    }
-                }
             }
-        }
-        else
-        {
-            if (item.Quality > 0)
-            {
-                if (item.Name == "Sulfuras, Hand of Ragnaros")
-                {
-                }
-                else
-                {
-                    item.Quality -= 1;
-                }
-            }
-        }
 
-        if (item.Name == "Sulfuras, Hand of Ragnaros")
-        {
-        }
-        else
-        {
             item.SellIn -= 1;
-        }
 
-        if (item.SellIn < 0)
-        {
-            if (item.Name == "Aged Brie")
+            if (item.SellIn < 0)
             {
                 IncreaseQuality(item);
             }
+        }
+        else
+        {
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
+
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.SellIn < 11)
+                        {
+                            IncreaseQuality(item);
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            IncreaseQuality(item);
+                        }
+                    }
+                }
+            }
             else
+            {
+                if (item.Quality > 0)
+                {
+                    if (item.Name == "Sulfuras, Hand of Ragnaros")
+                    {
+                    }
+                    else
+                    {
+                        item.Quality -= 1;
+                    }
+                }
+            }
+
+            if (item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+            }
+            else
+            {
+                item.SellIn -= 1;
+            }
+
+            if (item.SellIn < 0)
             {
                 if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
